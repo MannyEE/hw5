f0c5430bfe36b6893f4f67c822413d568d8c46f9
Emmanuel Ezirim <Eezirim23@gmail.com>
2024-04-09 07:50:02 -0700

    Completed wordle

--- wordle.cpp
+++ wordle.cpp
@@ -13,6 +13,12 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
+bool wordChecker(std::string str, std::string requirements, int place);
+std::set<std::string> wordleHelper(
+    const std::string& in,
+    const std::string& floating,
+    const std::set<std::string>& dict,
+    int letterPlace);
 
 
 // Definition of primary wordle function
@@ -22,7 +28,62 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    std::set<std::string>::iterator words;
+    std::set<std::string> cDict;
 
+    for(words = dict.begin(); words != dict.end(); ++words) {
+        bool valid = true;
+        for(int i = 0; i < (*words).length(); ++i){
+            if(isalpha((*words)[i]) == 0 || islower((*words)[i]) == 0) valid = false;
+        }
+        if (valid && ((*words).length() == in.length()) && wordChecker(*words, floating, 0)) cDict.insert(*words);
+    }
+
+    cDict = wordleHelper(in, floating, cDict, 0);
+
+    return cDict;
 }
 
 // Define any helper functions here
+std::set<std::string> wordleHelper(
+    const std::string& in,
+    const std::string& floating,
+    const std::set<std::string>& dict,
+    int letterPlace)
+{
+    if(letterPlace >= in.length()) return dict;
+
+    std::set<std::string>::iterator words;
+    std::set<std::string> cDict;
+
+    if (in[letterPlace] == '-') {
+        cDict = wordleHelper(in, floating, dict, letterPlace + 1);
+    
+    } else {
+
+        for(words = dict.begin(); words != dict.end(); ++words) {
+            if(in[letterPlace] != '-' && (*words)[letterPlace] == in[letterPlace]) cDict.insert(*words);
+        }
+        cDict = wordleHelper(in, floating, cDict, letterPlace + 1);
+    }
+    return cDict;
+}
+
+
+
+bool wordChecker(std::string str, std::string requirements, int place){
+    bool found = false;
+    std::string newStr = str;
+
+    if(place >= requirements.length()) return true;
+
+    for(int i = 0; i < str.length(); ++i){
+        if(requirements[place] == str[i]) {
+            found = true; 
+            newStr.erase(i, 1);
+            break;
+        }
+    }
+
+    return found && wordChecker(newStr, requirements, place + 1);
+}
